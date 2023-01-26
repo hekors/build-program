eabeb062456da4333224813a2a7e090c4eecb0ec
Yash Sehgal <yashsehgal@Yashs-MacBook-Air.local>
2023-01-26 23:29:49 +0530

    feat: container component added

---
+++ components/UIComponents/Container/index.tsx
@@ -0,0 +1,21 @@
+import { GenericComponentType } from "@/types/generic-type";
+
+const Container: React.FunctionComponent<GenericComponentType> = ({
+    children,
+    style,
+    id,
+    className
+}, props: any) => {
+    return (
+        <div 
+            className={className}
+            style={style}
+            id={id}
+            {...props}
+        >
+            {children}
+        </div>
+    )
+};
+
+export default Container;
